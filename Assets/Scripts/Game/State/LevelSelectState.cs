7ce9618831cb2b718d3bf1df2dacc01d7263046e
Shane Lillie <ignignokterr@gmail.com>
2021-07-03 11:27:16 -0700

    Notes

--- Assets/Scripts/Game/State/LevelSelectState.cs
+++ Assets/Scripts/Game/State/LevelSelectState.cs
@@ -2,5 +2,7 @@ namespace pdxpartyparrot.Game.State
 {
     public class LevelSelectState : GameState
     {
+        // TODO: we should hook in the level select UI here like we do for game over
+        // see ssj june 2021 for an example of that
     }
 }
